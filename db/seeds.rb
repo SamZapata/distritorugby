40f46d7efb4c2bd8694db3ee026d0167f17aa212
samzapata <legionario.zapata@gmail.com>
2020-04-02 17:57:56 -0500

    improve index and show III and add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Seed Information for Distrito Rugby 2020
+
+require 'csv'
+
+# first time just News
+CSV.foreach(Rails.root.join('seed/update_020420_news.csv'), headers: true) do |row|
+  n = News.new
+  n.title = row['title']
+  n.summary = row['summary']
+  n.talking = row['body']
+  n.image = row['image_src']
+  n.image_legend = row['image_legend']
+  n.emb_video = row['video_emb']
+  n.video_legend = row['video_legend']
+  n.tags = row['tags']
+  n.save
+  puts "The news #{n.title} was created successfully!"
+end
+
+# %w(
+#   news
+# ).each do |seed|
+#   puts "Loading seed information for: #{seed}"
+# end
