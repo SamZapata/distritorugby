56812c4b83b528957d0d2c36ee5d553ddd6b6d32
samzapata <legionario.zapata@gmail.com>
2020-06-01 13:24:49 -0500

    fix issues in news and add comment...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,8 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
+  # config.assets.js_compressor = :uglifier this seems that don't support ES6 which is the reason for the error. '`' character
+  # use this
   config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
