4bba45de3c102adbe34215faf3340675868ac518
samzapata <legionario.zapata@gmail.com>
2020-06-01 12:09:34 -0500

    remove asset precompile

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,5 @@ module Distritorugby
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.assets.initialize_on_precompile = false
   end
 end
